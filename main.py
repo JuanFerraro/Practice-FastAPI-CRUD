d2f409b7265365be02df28450cb3645e932c45b7
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-23 10:27:06 -0500

    Agregada lectura de archivos estaticos 💹

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 from models import Persona
 from utils import leer_personas, escribir_personas
+
 
 app = FastAPI()
 app.title = 'CRUD Python FastAPI'
 
+# Archivos estaticos:
+app.mount("/static", StaticFiles(directory="./public/static"), name="static")
+
 @app.get('/', tags = ['principal'])
 def principal():
-    return HTMLResponse('<h1>CRUD Python FastAPI</h1>')
+    return FileResponse('./public/static/html/index.html')
 
 @app.post('/personas', tags = ['personas'])
 def agregar_persona(persona: Persona):
