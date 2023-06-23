dd8a70c461f0d20ad4e58b05f9a53fb57fca529b
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-23 11:13:53 -0500

    Agregado formulario para añadir persona 📚

---
+++ public/static/js/script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('persona-form');
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Evita el envío del formulario
+
+        const formData = new FormData(form);
+        const personaData = {};
+
+        for (let [key, value] of formData.entries()) {
+            personaData[key] = value;
+        }
+
+        try {
+            const response = await fetch('/personas', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(personaData)
+            });
+
+            if (response.ok) {
+                // Persona agregada correctamente
+                alert('Persona agregada correctamente');
+            } else {
+                // Error en la respuesta del servidor
+                const responseData = await response.json();
+                alert(responseData.detail);
+            }
+        } catch (error) {
+            // Error de conexión o solicitud
+            console.error(error);
+            alert('Error en la solicitud');
+        }
+    });
